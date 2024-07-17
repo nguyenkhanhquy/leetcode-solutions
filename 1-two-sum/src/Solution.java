3c97fae6c10b6b4fd7e5030b70bfcca0b29c98c4
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-17 16:35:37 +0700

    refactor: 1-two-sum
    - Runtime: 2 ms - Beats: 97.91%
    - Memory: 45.03 MB - Beats: 13.18%

--- 1-two-sum/src/Solution.java
+++ 1-two-sum/src/Solution.java
@@ -2,15 +2,16 @@ import java.util.HashMap;
 
 public class Solution {
     public static int[] twoSum(int[] nums, int target) {
-
         HashMap<Integer, Integer> hashMap = new HashMap<>();
 
+        int num;
+        int tmp;
         for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            int temp = target - num;
+            num = nums[i];
+            tmp = target - num;
 
-            if (hashMap.containsKey(temp)) {
-                return new int[]{hashMap.get(temp), i};
+            if (hashMap.containsKey(tmp)) {
+                return new int[]{hashMap.get(tmp), i};
             }
 
             hashMap.put(num, i);
