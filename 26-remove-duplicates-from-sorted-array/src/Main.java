247e4c07d5e000a9fd9f8c059667d1eaa2af3b38
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-18 00:54:59 +0700

    refactor: 26-remove-duplicates-from-sorted-array
    - Runtime: 1 ms - Beats: 79.21%
    - Memory: 44.49 MB - Beats: 83.69%

--- 26-remove-duplicates-from-sorted-array/src/Main.java
+++ 26-remove-duplicates-from-sorted-array/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        int[] expectedNums = {0,1,2,3,4};
+        int[] nums = {0,0,1,1,1,2,2,3,3,4}; // Input array
+        int[] expectedNums = {0,1,2,3,4}; // The expected answer with correct length
 
-        int k = Solution.removeDuplicates(nums);
+        int k = Solution.removeDuplicates(nums); // Calls your implementation
 
-        assert k == expectedNums.length;
+        assert k == expectedNums.length : "k != expectedNums.length";
         for (int i = 0; i < k; i++) {
-            assert nums[i] == expectedNums[i];
+            assert nums[i] == expectedNums[i] : "nums[i] != expectedNums[i]";
         }
     }
 }
--- 26-remove-duplicates-from-sorted-array/src/Solution.java
+++ 26-remove-duplicates-from-sorted-array/src/Solution.java
@@ -1,11 +1,9 @@
 public class Solution {
     public static int removeDuplicates(int[] nums) {
         int k = 1;
-        int tmp = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            if (tmp != nums[i]) {
+            if (nums[i] != nums[k - 1]) {
                 nums[k] = nums[i];
-                tmp = nums[i];
                 k++;
             }
         }
