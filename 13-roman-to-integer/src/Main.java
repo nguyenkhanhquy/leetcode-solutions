23e7fd9ed9f97088d8c5911ed61c2abf2d0a5c42
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-17 15:49:22 +0700

    add: 13-roman-to-integer
    - Runtime: 5 ms - Beats: 56.69%
    - Memory: 44.98 MB - Beats: 13.74%

---
+++ 13-roman-to-integer/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = {"III", "LVIII", "MCMXCIV", "IV"};
+        for (String s : arr) {
+            System.out.println(Solution.romanToInt(s));
+        }
+    }
+}
